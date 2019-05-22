2941e95bb5df60e2fdbd91fb8976fe5d03c36d51
Aldane Purrier <aldanepur@gmail.com>
2019-05-22 16:56:40 -0400

    work on methods

--- main.js
+++ main.js
@@ -1,44 +1,66 @@
-// z_pokemon = []
+requestAPI = (pokemon) => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(this.responseText)
+
+  let pokemon = new Pokemon(data["stats"][5]["base_stat"],data["stats"][4]["base_stat"],
+  data["stats"][3]["base_stat"], data["abilities"][0]["ability"]["name"],
+  data["abilities"][1]["ability"]["name"])
+
+console.log(pokemon)
+
+}
+};
+xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${pokemon}/`, true);
+xhttp.send();
+}
 //
-// class Pokemon {
-//   constructor(hp, attack, defense, abilities) {
-//   this.hp = hp
-//   this.attack = attack
-//   this.abilities = []
-// }
+requestAPI("greninja")
+
+
+class Pokemon {
+  constructor(hp, attack, defense, ability1, ability2) {
+    this.hp = hp
+    this.attack = attack
+    this.defense = defense
+    this.ability1 = ability1
+    this.ability2 = ability2
+  }
+}
+
+class Zonal {
+  constructor() {
+  this.all()
+  this.get(name)
+  pokemon = []
+  }
+}
+
+
+//   const infernape = new Pokemon
 //
-// let Z = {
-//   pokemon: ["greninja", "infernape", "eevee"]
-// };
-//
-// let all = [, , ];
-// all.push();
-//
-// let get = [name];
-// get.push(name);
 //
 //
+// const infernape = new
+//
+      // console.log(data["stats"][5]["base_stat"])
+      // console.log(data["stats"][4]["base_stat"])
+      // console.log(data["stats"][3]["base_stat"])
+      // console.log(data["abilities"][0]["ability"]["name"])
+      // console.log(data["abilities"][1]["ability"]["name"])
+//
+//       let infernape = JSON.parse(this.responseText)
+//       console.log(data["stats"][5]["base_stat"])
+//       console.log(infer["stats"][4]["base_stat"])
+//       console.log(infernape["stats"][3]["base_stat"])
+//       console.log(infernape["abilities"][0]["ability"]["name"])
+//       console.log(infernape["abilities"][1]["ability"]["name"])
+
 //
 //
-// requestAPI = () => {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             let data = JSON.parse(this.responseText)
-//             user = {
-//                 name: data['name'],
-//                 username: data['login'],
-//                 image: data['avatar_url'],
-//                 repo_count: data['public_repos'],
-//                 followers: data['followers']
-//             }
-//         }
-//     };
-//     xhttp.open("GET", " https://fizal.me/pokeapi/api/v2/id/<id>.json", true);
-//     xhttp.send();
-// }
 //
-// requestAPI()
+//
 //
 // displayUser = () => {
 //     let cont = document.createElement('div')
@@ -54,45 +76,45 @@
 //     cont.appendChild(img)
 //     document.body.appendChild(cont)
 // }
-
-
-
-
-
-
-
-
-
-// Const ALL_CARS = []
-//   function loadDoc() {
-//     var xhttp = new XMLHttpRequest(); // create and XHR object -- request object
-//     xhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) { // everything takes place between this block, every thing in curly braces is considered a block
-//         let data = JSON.parse(this.responseText)
-//         for (item in data) {
-//           let car = new Car(item,
-//                             data[item].make
-//                             data[item].model
-//                             data[item].model_year
-//                             data[item].color)
-//            if (car.make =
-//              cars_sorted[data[item].make] =
 //
-//         }
-//        ;
-//       }
-//     };
 //
-//     xhttp.open("GET", "https://fizal.me/pokeapi/api/v2/id/<id>.json", true); // http verb, url
-//     xhttp.send(); //invoke XHR request (putting parentheses)
 //
-// class Car {
-//   constructor(vin, make, model, year, color) {
-//   this.vin = vin
-//   this.make = make
-//   this.make = model
-//   this.make = year
-//   this.make = color
 //
-// }
-// }
+//
+//
+//
+//
+//
+// // Const ALL_CARS = []
+// //   function loadDoc() {
+// //     var xhttp = new XMLHttpRequest(); // create and XHR object -- request object
+// //     xhttp.onreadystatechange = function() {
+// //       if (this.readyState == 4 && this.status == 200) { // everything takes place between this block, every thing in curly braces is considered a block
+// //         let data = JSON.parse(this.responseText)
+// //         for (item in data) {
+// //           let car = new Car(item,
+// //                             data[item].make
+// //                             data[item].model
+// //                             data[item].model_year
+// //                             data[item].color)
+// //            if (car.make =
+// //              cars_sorted[data[item].make] =
+// //
+// //         }
+// //        ;
+// //       }
+// //     };
+// //
+// //     xhttp.open("GET", "https://fizal.me/pokeapi/api/v2/id/<id>.json", true); // http verb, url
+// //     xhttp.send(); //invoke XHR request (putting parentheses)
+// //
+// // class Car {
+// //   constructor(vin, make, model, year, color) {
+// //   this.vin = vin
+// //   this.make = make
+// //   this.make = model
+// //   this.make = year
+// //   this.make = color
+// //
+// // }
+// // }
